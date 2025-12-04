72799cd8905e190c2334def257b2318e13c1b303
Uriel Werneck <uriel_werneck@hotmail.com>
2025-12-04 00:27:02 -0300

    chore(classes): set view to load all classes or filtered by id

--- classes/views.py
+++ classes/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from .models import SchoolClass
 
 
 # Create your views here.
+def classes(request):
+    if request.method == "POST":
+        pass
+    all_classes = SchoolClass.objects.all()
+    context = {'classes': all_classes}
+    return render(request, 'classes/classes.html', context=context)
+
+def class_by_id(request, pk):
+    if request.method == "POST":
+        pass
+    filtered_class = SchoolClass.objects.get(id=pk)
+    context = {'class': filtered_class}
+    return render(request, 'classes/class_by_id.html', context=context)

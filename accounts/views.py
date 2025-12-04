bd788141c641555cb4ce11bd187ad11c7ca7cc60
Uriel Werneck <uriel_werneck@hotmail.com>
2025-12-03 23:21:51 -0300

    feat(accounts): set login view and login path

---
+++ accounts/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def login(request):
+    if request.method == "POST":
+        pass
+    return render(request, 'accounts/login.html')

d83b1f4fb66eb92bfdc31d56f6e88810d7bd144d
Uriel Werneck <uriel_werneck@hotmail.com>
2025-12-09 14:06:23 -0300

    chore: delete website app

--- website/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import get_homepage
-
-urlpatterns = [
-    path('', get_homepage, name='homepage')
-]
--- website/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def get_homepage(request):
-    return render(request, 'website/homepage.html')

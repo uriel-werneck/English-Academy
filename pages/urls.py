2ccc960f3683b449b49a49073fcb0e2b29eb0a6d
Uriel Werneck <uriel_werneck@hotmail.com>
2025-12-11 22:53:11 -0300

    refactor(pages): rename homepage view to home

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import homepage
+from .views import home
+
+app_name = 'pages'
 
 urlpatterns = [
-    path('', homepage, name='homepage')
+    path('', home, name='home')
 ]
